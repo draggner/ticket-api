36a87d786acb46c4acc190008930d61b4fbbfeac
Aires Gonçalves <a.silva.goncalves@avanade.com>
2023-04-23 22:22:08 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' })
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Tickets API')
     .setVersion('1.0')

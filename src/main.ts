99c6562412067c0914413af4934ec317e3af31eb
Aires Gonçalves <a.silva.goncalves@avanade.com>
2023-04-23 22:21:13 -0300

    swagger configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Tickets API')
+    .setVersion('1.0')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
